31642c543ae54372f5bd942cef9d1694510f799e
JianChoi-Kor <wldks79@gmail.com>
2021-04-10 18:17:27 +0900

    jpa setting

---
+++ ex1/src/main/java/hellojpa/Member.java
@@ -0,0 +1,38 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+@TableGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE,
+            generator = "MEMBER_SEQ_GENERATOR")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
